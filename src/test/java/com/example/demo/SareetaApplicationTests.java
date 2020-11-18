90403aa4f35775de97754bf36118ee512580432c
BenSun <amortizedanalysis@gmail.com>
2020-11-18 22:08:49 +0100

    add JWT configs

--- src/test/java/com/example/demo/SareetaApplicationTests.java
+++ src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -7,7 +7,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SareetaApplicationTests {
+public class
+
+SareetaApplicationTests {
 
 	@Test
 	public void contextLoads() {
